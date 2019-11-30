a82922d92dbe85d07b5de3bdd170f201221460a4
Tim DiLauro <tim.di.lauro@gmail.com>
2019-11-30 00:13:36 -0500

    rename project

--- sailboatRacing/CoreData/PersistentContainerManager.swift
+++ sailboatRacing/CoreData/PersistentContainerManager.swift
@@ -20,7 +20,7 @@ class PersistentContainerManager {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
          */
-        let container = NSPersistentContainer(name: "jsonData")
+        let container = NSPersistentContainer(name: "sailboatRacing")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
